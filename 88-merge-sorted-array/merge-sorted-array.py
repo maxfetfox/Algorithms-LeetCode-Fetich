491fbb72c518fe91e9df3bd035640194f89c29bc
Maxim Fetich <sobakamau3@gmail.com>
2026-02-20 00:03:38 +0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (59.63%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -2,8 +2,9 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         i, j, ind = m - 1, n - 1, m + n - 1
         temp_lst = nums1[:m]
+        # временный список для хранения значений из num1
         while i >= 0 or j >= 0:
-            if j < 0:
+            if j < 0: # если один список пуст, проходимся по оставшемуся
                 nums1[ind] = temp_lst[i]
                 i -= 1
                 ind -= 1
@@ -11,7 +12,7 @@ class Solution:
                 nums1[ind] = nums2[j]
                 j -= 1
                 ind -= 1
-            elif temp_lst[i] >= nums2[j]:
+            elif temp_lst[i] >= nums2[j]: # >= в случае, если обе переменные равны друг другу
                 nums1[ind] = temp_lst[i]
                 i -= 1
                 ind -= 1
