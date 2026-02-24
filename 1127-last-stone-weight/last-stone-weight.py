adfe91a673d5d24ef4c773a66c451d33d96114f0
Maxim Fetich <sobakamau3@gmail.com>
2026-02-25 02:29:54 +0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (59.80%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -8,31 +8,32 @@ class Solution:
                     largest = left
                 else:
                     largest = i
-    
+
                 if right < size and nums[right] > nums[largest]:
                     largest = right
-    
+
                 if largest == i:
                     break
-    
+
                 nums[i], nums[largest] = nums[largest], nums[i]
-    
+
                 i = largest
-    
+
         def build_max_heap(nums: list) -> None:
             n = len(nums)
-    
+
             for i in range((n - 2) // 2, -1, -1):
                 max_heapify(nums, i, n)
-    
+
         def take_max(nums: list) -> int:
+            # забираем максимальный элемент
             maximal = nums[0]
             nums[0] = nums[-1]
             del nums[-1]
             if len(nums) > 0:
                 max_heapify(nums, 0, len(nums))
             return maximal
-    
+
         def add(nums: list, item: int) -> None:
             nums.append(item)
             i = len(nums) - 1
@@ -42,15 +43,15 @@ class Solution:
                     break
                 nums[j], nums[i] = nums[i], nums[j]
                 i = j
-    
+
         build_max_heap(stones)
-    
+
         while len(stones) > 1:
             y = take_max(stones)
             x = take_max(stones)
             if y != x:
                 add(stones, y - x)
-    
+
         if stones:
             return stones[0]
         return 0
