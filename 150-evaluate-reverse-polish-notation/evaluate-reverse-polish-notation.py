0ec02bb66a8abcdd6712e89d853acfc05c9804f4
Maxim Fetich <sobakamau3@gmail.com>
2026-03-20 00:55:21 +0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (65.25%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,26 +1,28 @@
 class Solution:
     def evalRPN(self, tokens: list[str]) -> int:
         stack = []
+        
         for item in tokens:
             if item == '+':
                 num1 = stack.pop()
                 num2 = stack.pop()
                 stack.append(num1 + num2)
+
             elif item == '*':
                 num1 = stack.pop()
                 num2 = stack.pop()
                 stack.append(num1 * num2)
+
             elif item == '-':
                 num1 = stack.pop()
                 num2 = stack.pop()
                 stack.append(num2 - num1)
+
             elif item == '/':
                 num1 = stack.pop()
                 num2 = stack.pop()
-                if abs(num2) < abs(num1):
-                    stack.append(0)
-                else:
-                    stack.append(int(num2 / num1))
+                stack.append(int(num2 / num1)) # только такая реализация, потому что целочисленное деление работает как floor
+
             else:
                 stack.append(int(item))
         return stack[0]
