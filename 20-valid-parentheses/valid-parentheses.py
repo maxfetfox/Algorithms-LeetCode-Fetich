542b534d38f9d8b762b0c730fe1e69d6865e4f8a
Maxim Fetich <sobakamau3@gmail.com>
2026-03-20 00:54:49 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,19 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0:
+        if len(s) % 2 != 0: # если количество скобок нечётно, у одной из скобок гарантированно нет пары
             return False
-        stack = []
+
+        stack = [] # сюда помещаются открывающие скобки
+
         for i in range(len(s)):
             if s[i] in '({[':
                 stack.append(s[i])
+
             elif len(stack) > 0:
                 if stack[-1] == '(' and s[i] == ')' or stack[-1] == '[' and s[i] == ']' or stack[-1] == '{' and s[
                     i] == '}':
-                    stack.pop()
+                    stack.pop() # если закрывающая скобка совпадает с последней открывающей, то проверка переходит к следующей паре
+
                 else:
-                    return False
+                    return False # в случае несовпадения – False
             else:
-                return False
-        if len(stack) > 0:
+                return False # если попалась сначала закрывающая скобка, затем открывающая, например: ')('
+        if len(stack) > 0: # если стек не пуст, значит у части скобок не оказалось пары
             return False
         return True
