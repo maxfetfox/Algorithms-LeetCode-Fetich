0d6a39e095f45157839d0e9c436d4a9cc4af7f2d
Maxim Fetich <sobakamau3@gmail.com>
2026-03-20 00:55:39 +0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (41.02%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,20 +1,20 @@
 class MyQueue:
     def __init__(self):
-        self.in_stack = []
-        self.out_stack = []
+        self.in_stack = [] # элементы добавляются сюда при push
+        self.out_stack = [] # сюда добавляются элементы из in_stack в обратном порядке и последний элемент используется для возвращения в pop
 
     def push(self, x: int) -> None:
         self.in_stack.append(x)
 
     def pop(self) -> int:
-        if len(self.out_stack) == 0:
+        if len(self.out_stack) == 0: # элементы добавляются только в том случае, если out_stack пуст. иначе просто возвращается первый элемент. это существенно ускоряет работу
             for _ in range(len(self.in_stack)):
                 self.out_stack.append(self.in_stack.pop())
             return self.out_stack.pop()
         return self.out_stack.pop()
 
     def peek(self) -> int:
-        if len(self.in_stack) == 0 or len(self.out_stack) > 0:
+        if len(self.in_stack) == 0 or len(self.out_stack) > 0: # пренебрегаем случаем если оба стека пусты
             return self.out_stack[-1]
         return self.in_stack[0]
 
