6f2a63475cf58248b0784b84d7546d74496856c3
Maxim Fetich <sobakamau3@gmail.com>
2026-04-02 20:58:53 +0500

    Time: 3 ms (34.71%) | Memory: 20.8 MB (97.34%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -7,9 +7,11 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         def BSTValidation(current, minimum, maximum):
+            # если дошли до пустого узла и до этого всякий раз сохранялась последовательность, возвращаем True
             if current is None:
                 return True
 
+            # проверка, входит ли значение в диапазон (minimum – минимальное значение в поддереве, maximum – максимальное). если не входит – False. далее рекурсивно запускаем проверку в следующих поддеревьях
             return False if not minimum < current.val < maximum else BSTValidation(current.left, minimum, current.val) and BSTValidation(current.right, current.val, maximum)
 
         return BSTValidation(root, -2 ** 31 - 1, 2 ** 31)
