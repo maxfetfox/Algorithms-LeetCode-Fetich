f751dfa223bc5a24e0ff60fd046d8159a16c265f
Maxim Fetich <sobakamau3@gmail.com>
2026-04-09 22:41:53 +0500

    Time: 936 ms (5.02%) | Memory: 21 MB (99.03%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,19 +1,20 @@
 class Solution:
     def networkDelayTime(self, times: list[list[int]], n: int, k: int) -> int:
-        distances = [float('inf')] * (n + 1)
+        distances = [float('inf')] * (n + 1) # для удобства в обоих списках счёт начинается с 1
         parents = [None] * (n + 1)
 
         distances[k] = 0
 
-        for _ in range(n - 1):
+        for _ in range(n - 1): # уменьшение расстояния
             for u, v, w in times:
                 if distances[u] != float('inf') and distances[v] > distances[u] + w:
                     distances[v] = distances[u] + w
                     parents[v] = u
 
-        for u, v, w in times:
-            if distances[u] != float('inf') and distances[v] > distances[u] + w:
+        for u, v, w in times: # если получится так, что расстояние можно ещё уменьшать после всех проходов,
+                              # то это бесконечный цикл и решения нет
+            if distances[v] > distances[u] + w:
                 return -1
 
-        time = max(distances[1:])
+        time = max(distances[1:]) # так как отсчёт начинается с 1, то элемент 0 всегда inf
         return time if time != float('inf') else -1
