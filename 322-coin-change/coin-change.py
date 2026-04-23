bc22a8a880c0f8e1d442b3f6f836816f1e8b5e21
Maxim Fetich <sobakamau3@gmail.com>
2026-04-23 20:24:52 +0500

    Time: 475 ms (76.72%) | Memory: 19.8 MB (40.94%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,17 +1,25 @@
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
+        # отмечаем каждую сумму недостижимой конечным числом монет
         dp = [float('inf')] * (amount + 1)
         dp[0] = 0
 
         if amount == 0:
             return 0
 
+        # отмечаем каждую сумму, которую можно достигнуть одной монетой как 1
         for coin in coins:
+            # проверка, чтобы индекс не выходил за пределы списка
             if coin <= amount:
                 dp[coin] = 1
 
+        # начинаем счёт с 1
         for i in range(1, len(dp)):
             for coin in coins:
+                # проверка, чтобы индекс не ломался
                 if i >= coin:
                     dp[i] = min(dp[i], dp[i - coin] + 1)
+                    # где i – текущий элемент, а i – coin – элемент, к которому если прибавить coin, получится элемент
+                    # с индексом i. + 1 к нему нужен, так как у i становится на одну монету больше, чтобы достигнуть его
+                    # сумму. min() выбирает более выгодный вариант
         return dp[amount] if dp[amount] != float('inf') else -1
